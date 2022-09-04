ee725a7135cbf86fd0e6488bf3c29037e0b05f72
RandomPrototypes <84458882+RandomPrototypes@users.noreply.github.com>
2022-09-04 12:45:46 +0900

    Update rpcamerainterface.rb

--- rpcamerainterface.rb
+++ rpcamerainterface.rb
@@ -5,14 +5,16 @@ class Rpcamerainterface < Formula
    sha256 "5f322cdba5b7b9d29a13a523d17f60c66ee4d80f54ff81828cadc962a70ae58e"
    license "ApacheV2"
    version "1.1.0"
+   
+   option "with-gstreamer", "Use gstreamer backend"
 
    depends_on "cmake" => :build
    depends_on "pkg-config" => :build
    depends_on "bufferedsocket"
-   depends_on "gstreamer"
-   depends_on "gst-plugins-base"
-   depends_on "gst-plugins-good"
-   depends_on "gst-plugins-bad"
+   depends_on "gstreamer" if build.with?('gstreamer')
+   depends_on "gst-plugins-base"  if build.with?('gstreamer')
+   depends_on "gst-plugins-good"  if build.with?('gstreamer')
+   depends_on "gst-plugins-bad"  if build.with?('gstreamer')
    depends_on "ffmpeg"
    depends_on "opencv"
 
@@ -20,6 +22,8 @@ class Rpcamerainterface < Formula
       args = std_cmake_args
       args << "-DUSE_OPENCV=ON"
       args << "-DBUILD_DEMO=ON"
+      args << "-DUSE_GSTREAMER=ON" if build.with?('gstreamer')
+      args << "-DUSE_GSTREAMER=OFF" if !build.with?('gstreamer')
       system "cmake", ".", *args
       system "make", "install"
    end
