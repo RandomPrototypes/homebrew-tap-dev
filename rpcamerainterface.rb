7c586ea2660d30a8ebeb8318455b816515be610c
RandomPrototypes <84458882+RandomPrototypes@users.noreply.github.com>
2022-09-04 12:57:16 +0900

    Update rpcamerainterface.rb

--- rpcamerainterface.rb
+++ rpcamerainterface.rb
@@ -1,8 +1,8 @@
 class Rpcamerainterface < Formula
    desc "Portable and unified interface for cameras."
    homepage "https://github.com/RandomPrototypes/RPCameraInterface"
-   url "https://github.com/RandomPrototypes/RPCameraInterface/archive/refs/tags/v1.2.0-dev-1.tar.gz"
-   sha256 "5f322cdba5b7b9d29a13a523d17f60c66ee4d80f54ff81828cadc962a70ae58e"
+   url "https://github.com/RandomPrototypes/RPCameraInterface/archive/refs/tags/v1.2.0-dev-2.tar.gz"
+   sha256 "4c2b0131526d1acc13c4c09debcf8fe9eeb181fd1400c8bd1e81b76e6a06de4b"
    license "ApacheV2"
    version "1.1.0"
    
