8eb82a0083eebe4fc48b817a8cf89b4b47247d40
RandomPrototypes <84458882+RandomPrototypes@users.noreply.github.com>
2022-09-04 14:50:08 +0900

    Create libquestmr.rb

---
+++ libquestmr.rb
@@ -0,0 +1,27 @@
+class Libquestmr < Formula
+   desc "Mixed reality library for quest 2"
+   homepage "https://github.com/RandomPrototypes/libQuestMR"
+   url "https://github.com/RandomPrototypes/libQuestMR/archive/refs/tags/v1.0.0-release.2.tar.gz"
+   sha256 "08881ddfb92231cc33dfdd4de32d7c076b2ea2a995599b88c8ea6ec482535ae6"
+   license "GPLV2"
+   version "1.0.0"
+
+   depends_on "cmake" => :build
+   depends_on "pkg-config" => :build
+   depends_on "bufferedsocket"
+   depends_on "rpcamerainterface"
+   depends_on "ffmpeg"
+   depends_on "opencv"
+   depends_on "onnxruntime"
+
+   def install
+      args = std_cmake_args
+      args << "-DUSE_OPENCV=ON"
+      args << "-DBUILD_DEMO=ON"
+      args << "-DUSE_RPCameraInterface=ON"
+      args << "-DUSE_FFMPEG=ON"
+      args << "-DUSE_ONNX_RUNTIME=ON"
+      system "cmake", ".", *args
+      system "make", "install"
+   end
+end
