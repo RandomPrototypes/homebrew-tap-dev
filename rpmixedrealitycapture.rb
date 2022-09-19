07e27e1ccd6318158075d75bb42cb0eb9bf2f25e
RandomPrototypes <84458882+RandomPrototypes@users.noreply.github.com>
2022-09-19 23:37:15 +0900

    Update rpmixedrealitycapture.rb

--- rpmixedrealitycapture.rb
+++ rpmixedrealitycapture.rb
@@ -12,10 +12,18 @@ class Rpmixedrealitycapture < Formula
    depends_on "rpcamerainterface"
    depends_on "libquestmr"
    depends_on "qt@5"
+   
+   resource "rvm_mobilenetv3_fp32" do
+      url "https://github.com/PeterL1n/RobustVideoMatting/releases/download/v1.0.0/rvm_mobilenetv3_fp32.onnx"
+      sha256 "88d4531297118f595bf2fd60f6f566aec2e559393802d1f436c380f0cbbd2828"
+   end
 
    def install
       args = std_cmake_args
       system "cmake", ".", *args
-      system "make", "install"
+      system "make"
+      resource("rvm_mobilenetv3_fp32").stage { libexec.install "resources/backgroundSub_data" }
+      libexec.install "RPMixedRealityCapture*"
+      bin.write_exec_script(libexec/"RPMixedRealityCapture")
    end
 end
