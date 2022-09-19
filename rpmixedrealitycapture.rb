310703beea0d32aa30b52b18beeb5f569b1116d5
RandomPrototypes <84458882+RandomPrototypes@users.noreply.github.com>
2022-09-20 08:38:48 +0900

    Update rpmixedrealitycapture.rb

--- rpmixedrealitycapture.rb
+++ rpmixedrealitycapture.rb
@@ -22,6 +22,8 @@ class Rpmixedrealitycapture < Formula
       args = std_cmake_args
       system "cmake", ".", *args
       system "make"
+      system "mkdir", libexec/"resources"
+      system "mkdir", libexec/"resources/backgroundSub_data"
       resource("rvm_mobilenetv3_fp32").stage { (libexec/"resources/backgroundSub_data").install "rvm_mobilenetv3_fp32.onnx"}
       libexec.install "RPMixedRealityCapture*"
       bin.write_exec_script(libexec/"RPMixedRealityCapture")
