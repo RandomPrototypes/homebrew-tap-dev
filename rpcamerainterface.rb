bba9f95925366a712cab82a6c527d573de3d6e7b
RandomPrototypes <84458882+RandomPrototypes@users.noreply.github.com>
2022-09-23 01:16:03 +0900

    Update rpcamerainterface.rb

--- rpcamerainterface.rb
+++ rpcamerainterface.rb
@@ -1,8 +1,8 @@
 class Rpcamerainterface < Formula
    desc "Portable and unified interface for cameras."
    homepage "https://github.com/RandomPrototypes/RPCameraInterface"
-   url "https://github.com/RandomPrototypes/RPCameraInterface/archive/refs/tags/v1.2.0-dev-3.tar.gz"
-   sha256 "5dd4f085d6c1a50ca161057243839ca46073cfa1a6d8958670870924db6f6ec6"
+   url "https://github.com/RandomPrototypes/RPCameraInterface/archive/refs/tags/v1.2.0-dev-4.tar.gz"
+   sha256 "a5889a71931d6829464d1509e0999c8ffea7aa0f87f308ab49002180acb92348"
    license "ApacheV2"
    version "1.2.0"
    
@@ -21,10 +21,13 @@ class Rpcamerainterface < Formula
    def install
       args = std_cmake_args
       args << "-DUSE_OPENCV=ON"
-      args << "-DBUILD_DEMO=ON"
+      args << "-DBUILD_DEMO=OFF"
       args << "-DUSE_GSTREAMER=ON" if build.with?('gstreamer')
       args << "-DUSE_GSTREAMER=OFF" if !build.with?('gstreamer')
       system "cmake", ".", *args
+      system "make", "install"
+      system "cd", "demo"
+      system "cmake", ".", *std_cmake_args
       system "make", "install"
    end
 end
