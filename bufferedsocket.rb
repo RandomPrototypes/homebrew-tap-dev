9c03fc8f8f4085ecd23765c11a525dc6dc9eae6f
RandomPrototypes <84458882+RandomPrototypes@users.noreply.github.com>
2022-09-27 01:47:19 +0900

    Update bufferedsocket.rb

--- bufferedsocket.rb
+++ bufferedsocket.rb
@@ -1,15 +1,17 @@
 class Bufferedsocket < Formula
    desc "A multiplatform library for socket communication with buffering"
    homepage "https://github.com/RandomPrototypes/BufferedSocket"
-   url "https://github.com/RandomPrototypes/BufferedSocket/archive/refs/tags/v1.1.0-dev.tar.gz"
-   sha256 "78145f3cdcbf7ee4a6c48ef54e01c1bb5871c14d8d48af227ba5faea5fd6274f"
+   url "https://github.com/RandomPrototypes/BufferedSocket/archive/refs/tags/v1.1.2-dev.tar.gz"
+   sha256 "11dedef67ef5d3459c76ad0277c215363eaceb43c89e2aac55882fa998a3807a"
    license "ApacheV2"
-   version "1.1.0"
+   version "1.1.2"
 
    depends_on "cmake" => :build
 
    def install
-      system "cmake", ".", *std_cmake_args
+      args = std_cmake_args
+      args << "-DUSE_POLL_INSTEAD_OF_SELECT=ON"
+      system "cmake", ".", *args
       system "make", "install"
    end
 end
