39493ed406d88170115c5cd43ddc47fc1f4a40f0
RandomPrototypes <84458882+RandomPrototypes@users.noreply.github.com>
2023-01-19 00:35:20 +0900

    Create screen_capture_lite.rb

---
+++ screen_capture_lite.rb
@@ -0,0 +1,17 @@
+class Screen_capture_lite < Formula
+   desc "Cross-platform screen and window capturing library"
+   homepage "https://github.com/smasherprog/screen_capture_lite"
+   url "https://github.com/smasherprog/screen_capture_lite/archive/refs/tags/17.1.613.zip"
+   sha256 "e1cc327643bea2eea5854954d35be0c838ab23821cd89011ea6fc620db0b7032"
+   license "MIT"
+   version "17.1.613"
+
+   depends_on "cmake" => :build
+
+   def install
+      args = std_cmake_args
+      args << "-DBUILD_CSHARP=OFF -DBUILD_EXAMPLE=OFF"
+      system "cmake", ".", *args
+      system "make", "install"
+   end
+end
